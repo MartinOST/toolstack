73caae8bb457c4a7a430c756759f5b8c81e63f0a
Martin Osterloh <osterlohm@ainfosec.com>
2015-11-30 09:53:13 -0500

    changes to main, and makefile

--- xlshim/src/xlshim.h
+++ xlshim/src/xlshim.h
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 /* As per defintion, any application that uses LibXL must define LIBXL_API_VERSION */
-
-#define LIBXL_API_VERSION 0x403040 /* Xen 4.3.4 */
+#ifndef LIBXL_API_VERSION
+#define LIBXL_API_VERSION 0x040300 /* Xen 4.3.4 */
+#endif
 
 #include <libxl.h>
 
